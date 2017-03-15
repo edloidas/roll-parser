37412b1c94a5238c2293a768ad3b3724e3765c68
Mikita Taukachou <edloidas@gmail.com>
2017-03-15 13:11:20 +0300

    Add simple grammar for rolls with limits
    Added simple grammar regexp to parse rolls with bottom and upper limits.
    Added tests.

--- test/grammar/grammar.simple.spec.js
+++ test/grammar/grammar.simple.spec.js
@@ -1,7 +1,11 @@
 const grammar = require( '../../src/grammar' );
 const util = require( '../util' );
 
-const [ single, double, triple ] = [ ...grammar.simple ];
+const [
+  single, double, triple,
+  tripleBottomLimit,
+  tripleBottomAndUpperLimit,
+] = [ ...grammar.simple ];
 
 const testRolls = ( regexp, desc, validRolls, invalidRolls ) => {
   describe( desc, () => {
@@ -16,20 +20,34 @@ const testRolls = ( regexp, desc, validRolls, invalidRolls ) => {
 testRolls(
   single,
   'One value roll',
-  [ '1', '123456789' ],
+  [ '0', '123456789' ],
   [ '-2' ],
 );
 
 testRolls(
   double,
   'Two values roll',
-  [ '1 2', '123    12345' ],
+  [ '1  2', '12 123' ],
   [ '0', '-2', '2 -20', '-10 1', '-10 -20' ],
 );
 
 testRolls(
   triple,
   'Three values roll',
-  [ '1 2 3', '1 20 0', '2  10  +11', '2  10  -11' ],
+  [ '1 2 3', '2  10  +11', '2  10  -11' ],
   [ '0', '-2', '2 -20', '-10 1', '-10 -20', '2 -20 1', '-10 1 2', '-10 -20 3' ],
+);
+
+testRolls(
+  tripleBottomLimit,
+  'Three values with upper limit roll',
+  [ '1 2 3 1', '2  10  +11  5', '2  10  -11  10' ],
+  [ '1 2 3 -1', '1 2 3 +1' ],
+);
+
+testRolls(
+  tripleBottomAndUpperLimit,
+  'Three values with bottom and upper limit roll',
+  [ '1 2 3 1 3', '2  10  +11  5  30', '2  10  -11  5  10' ],
+  [ '1 2 3 1 -1', '1 2 3 1 +1' ],
 );
