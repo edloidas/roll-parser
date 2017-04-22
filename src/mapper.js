9594308c3e77e64433d9b6742f01e48e3597a8b0
edloidas <edloidas@gmail.com>
2017-04-22 21:23:13 +0300

    Add mapper to map parser result to `Roll` object

---
+++ src/mapper.js
@@ -0,0 +1,22 @@
+const Roll = require( './roll' );
+
+const toInteger = srt => parseInt( srt || 0, 10 );
+
+/**
+ * @param {Array} result - result of the parse excecution.
+ */
+function map( result ) {
+  if ( !result || result.length < 2 ) {
+    return null;
+  }
+
+  const values = result.slice( 1, 6 ).map( toInteger );
+
+  if ( values.length === 1 ) {
+    return new Roll( values[ 0 ]);
+  }
+
+  return new Roll( values[ 1 ], values[ 0 ], ...values.slice( 2, 5 ));
+}
+
+module.exports = map;
---
+++ test/mapper.spec.js
@@ -0,0 +1,28 @@
+const { parseSimple, parseClassic } = require( '../src/parser' );
+const map = require( '../src/mapper' );
+
+describe( 'Map parser result:', () => {
+  test( 'Should map falsy values to `null`.', () => {
+    expect( map( null )).toBeNull();
+  });
+
+  test( 'Should set dice for single value roll.', () => {
+    expect( map( parseSimple( '10' ))).toMatchObject({ dice: 10 });
+  });
+
+  test( 'Should map dice to second value and count to first value.', () => {
+    expect( map( parseSimple( '2 10' ))).toMatchObject({ dice: 10, count: 2 });
+  });
+
+  test( 'Should correctly map from 3 to 5 values', () => {
+    expect( map( parseSimple( '2 10 -2' ))).toMatchObject({ dice: 10, count: 2, modifier: -2 });
+    expect( map( parseSimple( '2 10 +2 2' ))).toMatchObject({ dice: 10, count: 2, modifier: 2, bottom: 2 });
+    expect( map( parseSimple( '2 10 2 2 17' ))).toEqual({ dice: 10, count: 2, modifier: 2, bottom: 2, top: 17 });
+    expect( map( parseClassic( '2d10-2 (,17)' ))).toEqual({ dice: 10, count: 2, modifier: -2, bottom: 0, top: 17 });
+  });
+
+  test( 'Should trim excess values', () => {
+    const result = { dice: 10, count: 2, modifier: -2, bottom: 2, top: 17 };
+    expect( map([ 'skipped', '2', '10', '-2', '2', '17', '9' ])).toEqual( result );
+  });
+});
