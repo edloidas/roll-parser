c73f0a0d5e7b3bffa9701a69713d8dd35f3a922f
edloidas <edloidas@gmail.com>
2017-05-01 23:57:50 +0300

    Add WoD parser test

---
+++ test/parser/parser.wod.spec.js
@@ -0,0 +1,9 @@
+const { parseWod } = require( '../../src/parser' );
+const { testParse } = require( '../util' );
+
+testParse(
+  parseWod,
+  'Parse `WoD` notation:',
+  [ 'd10', 'd10!', '1d10!', 'd10>8', 'd10>8f1', 'd10!>8f1', '1d10>8f1', '1d10!>8f1' ],
+  [ '10', '2d10+1' ],
+);
