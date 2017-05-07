30be4a2478d04b801e82e018ae57b1fc9d07ee43
edloidas <edloidas@gmail.com>
2017-05-08 01:06:19 +0300

    Add `WodRoll.toString` tests

---
+++ test/object/wodroll/wodroll.toString.spec.js
@@ -0,0 +1,7 @@
+const WodRoll = require( '../../../src/object/WodRoll' );
+
+describe( 'WodRoll.toString:', () => {
+  test( 'Should generate roll `2d20!>9f2`.', () => {
+    expect( new WodRoll( 20, 2, true, 9, 2 ).toString()).toEqual( '2d20!>9f2' );
+  });
+});
