8d60aae437be4dfcb7549dc335e2ad4cb16cff51
edloidas <edloidas@gmail.com>
2017-05-08 13:55:05 +0300

    Add `WodRoll` tests

--- test/object/roll/roll.generation.spec.js
+++ test/object/roll/roll.generation.spec.js
@@ -11,17 +11,17 @@ describe( 'Roll generation:', () => {
     expect( new Roll( 10, 2, -2, 2, 9 )).toEqual( result );
   });
 
-  test( 'Should replace zero borders with 1 for roll `4d6+1 (0,0)`.', () => {
+  test( 'Should replace zero top borders with dice value for roll `4d6+1 (0,0)`.', () => {
     const result = { dice: 6, count: 4, modifier: 1, bottom: 0, top: 6 };
     expect( new Roll( 6, 4, 1, 0, 0 )).toEqual( result );
   });
 
-  test( 'Should fix border, if bottom limmit is bigger than upper limit`.', () => {
+  test( 'Should fix border, if bottom limmit is bigger than upper limit.', () => {
     const result = { dice: 12, count: 3, modifier: -4, bottom: 5, top: 5 };
     expect( new Roll( 12, 3, -4, 9, 5 )).toEqual( result );
   });
 
-  test( 'Should fix border, if top limmit is bigger than dice value`.', () => {
+  test( 'Should fix border, if top limmit is bigger than dice value.', () => {
     const result = { dice: 12, count: 3, modifier: -4, bottom: 5, top: 12 };
     expect( new Roll( 12, 3, -4, 5, 17 )).toEqual( result );
   });
