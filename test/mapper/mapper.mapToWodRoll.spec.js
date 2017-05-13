6335c9e846abde8a38803fd0d813038a49a00a16
edloidas <edloidas@gmail.com>
2017-05-13 23:44:52 +0300

    Remove unused tests

--- test/mapper/mapper.mapToWodRoll.spec.js
+++ test/mapper/mapper.mapToWodRoll.spec.js
@@ -18,10 +18,8 @@ describe( 'Map parser result to WodRoll:', () => {
     expect( mapToWodRoll( parseWod( '2d20!' ))).toMatchObject({ dice: 20, count: 2, again: true });
     expect( mapToWodRoll( parseWod( '2d20>7' ))).toMatchObject({ dice: 20, count: 2, success: 7 });
     expect( mapToWodRoll( parseWod( '2d20>7f2' ))).toMatchObject({ dice: 20, count: 2, success: 7, fail: 2 });
-    // expect( mapToWodRoll( parseWod( '2d20f2' ))).toMatchObject({ dice: 20, count: 2, fail: 2 });
     expect( mapToWodRoll( parseWod( 'd20!' ))).toMatchObject({ dice: 20, again: true });
     expect( mapToWodRoll( parseWod( 'd20!>7' ))).toMatchObject({ dice: 20, again: true, success: 7 });
-    // expect( mapToWodRoll( parseWod( 'd20!f2' ))).toMatchObject({ dice: 20, again: true, fail: 2 });
     expect( mapToWodRoll( parseWod( 'd20>7f2' ))).toMatchObject({ dice: 20, success: 7, fail: 2 });
     expect( mapToWodRoll( parseWod( 'd20!>7f2' ))).toMatchObject({ dice: 20, again: true, success: 7, fail: 2 });
     expect( mapToWodRoll( parseWod( '2d20!>7f2' ))).toMatchObject({ dice: 20, count: 2, again: true, success: 7, fail: 2 });
