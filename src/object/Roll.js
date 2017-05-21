3b307a2a926ed57560010dee0c2a9cd34b226c61
edloidas <edloidas@gmail.com>
2017-05-21 23:38:48 +0300

    :memo: Add documentation for roll classes

--- src/object/Roll.js
+++ src/object/Roll.js
@@ -3,6 +3,18 @@ const { classicNotation } = require( '../stringifier' );
 
 const positiveInteger = fixInvalid( 1 );
 
+/**
+ * A class that represents a dice roll from D&D setting
+ * @class
+ * @classdesc A class that represents a dice roll from D&D setting
+ * @since v2.0.0
+ * @param {Number} dice - A number of dice faces
+ * @param {Number} count - A number of dices
+ * @param {Number} modifier - A modifier, that should be added/sustracted from result
+ * @param {Number} bottom - A non-standard value, below wich dice roll shouldn't be count
+ * @param {Number} top - A non-standard value, above wich dice roll shouldn't be count
+ * @see WodRoll
+ */
 function Roll( dice = 20, count = 1, modifier = 0, bottom, top ) {
   this.dice = positiveInteger( dice );
   this.count = positiveInteger( count );
--- src/object/WodRoll.js
+++ src/object/WodRoll.js
@@ -3,6 +3,18 @@ const { wodNotation } = require( '../stringifier' );
 
 const positiveInteger = fixInvalid( 1 );
 
+/**
+ * A class that represents a dice roll from World of Darkness setting
+ * @class
+ * @classdesc A class that represents a dice roll from World of Darkness setting
+ * @since v2.0.0
+ * @param {Number} dice - A number of dice faces
+ * @param {Number} count - A number of dices
+ * @param {Boolean} again - A flag for "10 Again" rolls policy
+ * @param {Number} success - A minimum value, that counts as success
+ * @param {Number} fail - A maximum value, that counts as failure
+ * @see Roll
+ */
 function WodRoll( dice = 10, count = 1, again = false, success = 6, fail = 1 ) {
   this.dice = positiveInteger( dice );
   this.count = positiveInteger( count );
