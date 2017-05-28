174214fe710dc73a08d3afc86bc64799a07ddef8
edloidas <edloidas@gmail.com>
2017-05-28 13:49:23 +0300

    Remove excess grammar
    Removed limits from simple and classic rolls.

--- test/grammar/grammar.classic.spec.js
+++ test/grammar/grammar.classic.spec.js
@@ -3,8 +3,6 @@ const testRolls = require( '../util' ).testRolls;
 
 const [
   classicRolls,
-  rollsWithBottomLimit,
-  rollsWithLimits,
 ] = [ ...grammar.classic ];
 
 testRolls(
@@ -26,32 +24,4 @@ testRolls(
   'Rolls with modifier: "YdX±Z"',
   [ '20d20+17', '2D6-1', 'd20+3' ],
   [ '1d20 +0', '2d10 5', '1D6-' ],
-);
-
-testRolls(
-  rollsWithBottomLimit,
-  'Rolls with bottom limit: "YdX (A)" or "YdX (A,)"',
-  [ '20d20 (6)', '2D6  ( 11 )', 'd20 (4)' ],
-  [ '1d20 ()', '1d20 (,6)' ],
-);
-
-testRolls(
-  rollsWithLimits,
-  'Rolls with limits (upper or both): "YdX (,B)" or "YdX (A,B)"',
-  [ '20d20 (,6)', '2D6  ( , 11 )', '20d20 (6,11)', '2D6  ( 6 , 11 )', 'd20 (,17)', 'd20 (4,17)' ],
-  [ '1d20 (6,)' ],
-);
-
-testRolls(
-  rollsWithBottomLimit,
-  'Rolls with modifier and bottom limit: "YdX±Z (A) or "YdX±Z (A,)""',
-  [ '20d20+17 (6)', '2D6-1  ( 11 )', 'd20+3 (4)' ],
-  [ '1d20+0 ()', '1d20+0 (,6)' ],
-);
-
-testRolls(
-  rollsWithLimits,
-  'Rolls with modifier and limits (upper or both): "YdX±Z (,B)" or "YdX±Z (A,B)"',
-  [ '20d20+17 (,6)', '2D6-1  ( , 11 )', '20d20+17 (6,11)', '2D6-1  ( 6 , 11 )', 'd20+4 (,17)', 'd20+4 (4,17)' ],
-  [ '1d20+0 (6,)' ],
 );
--- test/grammar/grammar.simple.spec.js
+++ test/grammar/grammar.simple.spec.js
@@ -1,42 +1,25 @@
 const grammar = require( '../../src/grammar' );
 const testRolls = require( '../util' ).testRolls;
 
-const [
-  singleOrDouble,
-  tripleWithLimits,
-] = [ ...grammar.simple ];
+const [ singleToTriple ] = [ ...grammar.simple ];
 
 testRolls(
-  singleOrDouble,
+  singleToTriple,
   'One value roll',
   [ '0', '123456789' ],
   [ '-2' ],
 );
 
 testRolls(
-  singleOrDouble,
+  singleToTriple,
   'Two values roll',
   [ '1  2', '12 123' ],
   [ '-2', '2 -20', '-10 1', '-10 -20' ],
 );
 
 testRolls(
-  tripleWithLimits,
+  singleToTriple,
   'Three values roll',
   [ '1 2 3', '2  10  +11', '2  10  -11' ],
-  [ '0', '-2', '2 20', '2 -20', '-10 1', '-10 -20', '2 -20 1', '-10 1 2', '-10 -20 3', '2 20+3', '2 20-3' ],
-);
-
-testRolls(
-  tripleWithLimits,
-  'Three values with upper limit roll',
-  [ '1 2 3 1', '2  10  +11  5', '2  10  -11  10' ],
-  [ '1 2 3 -1', '1 2 3 +1' ],
-);
-
-testRolls(
-  tripleWithLimits,
-  'Three values with bottom and upper limit roll',
-  [ '1 2 3 1 3', '2  10  +11  5  30', '2  10  -11  5  10' ],
-  [ '1 2 3 1 -1', '1 2 3 1 +1' ],
+  [ '-2', '2 -20', '-10 1', '-10 -20', '2 -20 1', '-10 1 2', '-10 -20 3', '2 20+3', '2 20-3', '1 2 3 4' ],
 );
